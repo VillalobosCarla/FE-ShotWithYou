6ba333928d5b3b2380e16cf594283fa3f8ac65e6
BatStateU <carlavillalobos00@gmail.com>
2024-06-20 12:12:38 +0800

    act 1

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,59 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MaterialApp(
-    home:Scaffold(
-      appBar: AppBar(
-        title: Center(
-          child: Text('ShotWithYou'),
-        ),
-        backgroundColor: Colors.tealAccent,
-      ),
-      body: Center(
-        child: Image.network('https://img.freepik.com/free-photo/glass-red-wine-bar-counter_107420-65846.jpg'),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        backgroundColor:  Colors.tealAccent,
-        child: Icon(
-            Icons.add,
-            color: Colors.pinkAccent,
+void main() => runApp(MaterialApp(
+  home: Scaffold(
+    backgroundColor: Colors.white,
+    appBar: AppBar(
+      title: Text('User Profile'),
+      backgroundColor: Colors.teal,
+      centerTitle: true,
+    ),
+      body: Padding(
+        padding: EdgeInsets.all(20.0),
+        child: Column(
+         crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text(
+              'NAME:',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 24.0
+              ),
+            ),
+            Text(
+              'Carla M. Villalobos',
+              style: TextStyle(
+                fontSize: 20.0
+              ),
+            ),
+            Text(
+              'YEAR:',
+              style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 24.0
+              ),
+            ),
+            Text(
+              'Third Year',
+              style: TextStyle(
+                  fontSize: 20.0
+              ),
+            ),
+            Text(
+              'EMAIL:',
+              style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 24.0
+              ),
+            ),
+            Text(
+              'carlavillalobos00@gmail.com',
+              style: TextStyle(
+                  fontSize: 20.0
+              ),
+            ),
+          ],
         ),
     ),
-    )
-  ));
-}
-
+  ),
+));
